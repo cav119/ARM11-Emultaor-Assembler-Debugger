b8f4756aa5b21f1b3d88ba785a82ee9a9f60bbee
cav119 <carlos.valencia-vargas19@imperial.ac.uk>
2020-05-22 17:23:10 +0100

    getters and setters for CPSR

--- src/emulate.c
+++ src/emulate.c
@@ -9,6 +9,7 @@
 #define MEMORY_SIZE 65536
 #define PC 15
 #define CPSR 16
+#define MAX_INDEX 31
 
 // CpuState struct
 typedef struct {
@@ -29,6 +30,8 @@ typedef struct {
 	instruction_type type;	
 } instruction;
 
+typedef enum {N, Z, C, V} flag;
+
 
 // make utilities.c and add this
 // Utility func: checks if pointer is null and exits with error message if so
@@ -45,14 +48,12 @@ CpuState *cpu_state_init(void) {
 	check_ptr_not_null(cpu_state, "Could not allocate cpu state memory");
 
 	// allocate and init registers to 0
-	uint8_t *allocated_regs = malloc(NUM_REGISTERS * sizeof(uint32_t));
-	allocated_regs = memset(allocated_regs, 0, NUM_REGISTERS * sizeof(uint32_t));
+	uint8_t *allocated_regs = calloc(NUM_REGISTERS, sizeof(uint32_t));
 	check_ptr_not_null(allocated_regs, "Could not allocate and initialise registers");
 	cpu_state->registers = (uint32_t *) allocated_regs;
 
 	// allocate and init memory to 0
-	uint8_t *allocated_memory = malloc(MEMORY_SIZE);
-	allocated_memory = memset(allocated_memory, 0, MEMORY_SIZE);
+	uint8_t *allocated_memory = calloc(MEMORY_SIZE, sizeof(uint8_t));
 	check_ptr_not_null(allocated_memory, "Could not allocate and initialise RAM memory");
 	cpu_state->memory = (uint8_t *) allocated_memory;
 
@@ -62,6 +63,8 @@ CpuState *cpu_state_init(void) {
 // Frees the allocated memory in the CpuState
 void cpu_state_free(CpuState *cpu_state){
 	free(cpu_state->memory);
+	free(cpu_state->registers);
+	free(cpu_state);
 }
 
 // Prints the current state of all registers for debugging
@@ -74,6 +77,24 @@ void print_registers(CpuState *cpu_state){
 		cpu_state->registers[CPSR]
 	);
 }
+
+//sets given flag in CPSR to (0 /1);
+void set_flag(CpuState *cpuState, flag flag, bool set){
+    if (flag > 3){
+        printf("wrong flag");
+        exit(EXIT_FAILURE);
+    }
+    int mask = (1 << (MAX_INDEX - flag));
+    cpuState->registers[CPSR] = set ?
+            cpuState->registers[CPSR] | mask : cpuState -> registers[CPSR] & ~(mask);
+}
+
+//gets the value of the given flag
+bool get_flag(CpuState *cpuState, flag flag){
+    int mask = (1 << (MAX_INDEX - flag));
+    return cpuState->registers[CPSR] & mask;
+}
+
 
 // Prints the current state of memory for debugging
 void print_memory(CpuState *cpu_state, int from, int to){
@@ -89,30 +110,30 @@ void print_first_instruction(CpuState *cpu_state) {
 	uint8_t byte3 = cpu_state->memory[1];
 	uint8_t byte4 = cpu_state->memory[0];
 
-	printf("First instruction is: %x %x %x %x\n", byte1, byte2, byte3, byte4);
+	printf("First instruction is: %.2x %.2x %.2x %.2x\n", byte1, byte2, byte3, byte4);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
 	assert(argc == 2);
 	printf("%s\n", argv[1]);
 
 	CpuState *cpu_state = cpu_state_init();
 
 	// Load binary file data into CPU Memory
-	FILE *prog_file = fopen(argv[1], "rb");  // Open the file in binary mode
+	FILE *prog_file = fopen(argv[1], "rb");                // Open the file in binary mode
 	check_ptr_not_null(prog_file, "Could not open file.");
 
-	fseek(prog_file, 0, SEEK_END);      // Jump to the end of the file
-	uint64_t file_length = ftell(prog_file);     // Get the current byte offset in the file
-	rewind(prog_file);                  // Jump back to the beginning of the file
+	fseek(prog_file, 0, SEEK_END);                         // Jump to the end of the file
+	uint64_t file_length = ftell(prog_file);               // Get the current byte offset in the file
+	rewind(prog_file);                                     // Jump back to the beginning of the file
 
-	assert(file_length <= MEMORY_SIZE); // Program data has to fit into memory
-	fread(cpu_state->memory, file_length, 1, prog_file); // Read in the entire file
+	assert(file_length <= MEMORY_SIZE);                    // Program data has to fit into memory
+	fread(cpu_state->memory, file_length, 1, prog_file);   // Read in the entire file
 	fclose(prog_file); // Close the file
 
 
-	print_registers(cpu_state);
-	print_memory(cpu_state, 4, 16);
+    print_registers(cpu_state);
+	print_memory(cpu_state, 0, 16);
 	print_first_instruction(cpu_state);
 
 	cpu_state_free(cpu_state);
