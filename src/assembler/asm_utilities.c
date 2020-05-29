ba4844e8afd60f52780a8adcbdffec2774290f95
Alex Constantin-Gomez <ac3419@ic.ac.uk>
2020-05-29 21:53:54 +0100

    Started implementing SDT instruction encoding, added some util functions to utilities

---
+++ src/assembler/asm_utilities.c
@@ -0,0 +1,44 @@
+#include <string.h>
+#include <stdlib.h>
+#include "asm_utilities.h"
+
+// char **get_instr_tokens(char instr[]) {
+//     char **words = calloc(5, 512);
+//     char *arr = strtok(instr, " ,:\n");
+//     int i = 0;
+//     while (arr != NULL) {
+//         words[i] = arr;
+//         arr = strtok(NULL, " ,:\n");
+//         i ++;
+//     }
+//     return words;
+// }
+
+// int main() {
+//     char x[] = "hello there!";
+//     printf("%s\n", substring(x, 1, 3));
+//     return 0;
+// }
+
+
+uint32_t process_mask(uint32_t n, uint8_t start_pos, uint8_t end_pos) {
+    uint32_t mask = (1 << (end_pos + 1 - start_pos)) - 1;
+    return (n >> start_pos) & mask;
+}
+
+
+void print_bits(uint32_t x) {
+    int i;
+    uint32_t mask = 1 << 31;
+
+    for (i=0; i<32; ++i) {
+        if ((x & mask) == 0) {
+            printf("0");
+        }
+        else {
+            printf("1");
+        }
+        x = x << 1;
+    }
+    printf("\n");
+}
---
+++ src/assembler/parser.h
@@ -0,0 +1,16 @@
+// #ifndef PARSER_H
+// #define PARSER_H
+
+// // change return types
+
+// // Creates a symbol table from the assembly file
+// SymbolTable *build_symbol_table(const char **lines);
+
+// // Generates the binary code for each assembly instruction
+// void generate_binary_code(const char **lines, SymbolTable table);
+
+// // Parse each token from a line (instruction)
+// void decode_instruction(const char *instr);
+
+
+// #endif
