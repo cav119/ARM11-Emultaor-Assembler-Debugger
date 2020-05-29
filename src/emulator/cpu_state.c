9184153b81462e99265c99130ad20bce26f7c1e4
Alex Constantin-Gomez <ac3419@ic.ac.uk>
2020-05-29 15:58:53 +0100

    Changed tabs to spaces and recovered goto fix

--- src/emulator/cpu_state.c
+++ src/emulator/cpu_state.c
@@ -9,122 +9,122 @@
 
 
 CpuState *cpu_state_init(void) {
-	CpuState *cpu_state = (CpuState *) malloc(sizeof(CpuState));
-	check_ptr_not_null(cpu_state, "Could not allocate cpu state memory");
+    CpuState *cpu_state = (CpuState *) malloc(sizeof(CpuState));
+    check_ptr_not_null(cpu_state, "Could not allocate cpu state memory");
 
-	// allocate and init registers to 0
-	uint8_t *allocated_regs = calloc(NUM_REGISTERS, sizeof(uint32_t));
-	check_ptr_not_null(allocated_regs, "Could not allocate and initialise registers");
-	cpu_state->registers = (uint32_t *) allocated_regs;
+    // allocate and init registers to 0
+    uint8_t *allocated_regs = calloc(NUM_REGISTERS, sizeof(uint32_t));
+    check_ptr_not_null(allocated_regs, "Could not allocate and initialise registers");
+    cpu_state->registers = (uint32_t *) allocated_regs;
 
-	// allocate and init memory to 0
-	uint8_t *allocated_memory = calloc(MEMORY_SIZE, sizeof(uint8_t));
-	check_ptr_not_null(allocated_memory, "Could not allocate and initialise RAM memory");
-	cpu_state->memory = (uint8_t *) allocated_memory;
+    // allocate and init memory to 0
+    uint8_t *allocated_memory = calloc(MEMORY_SIZE, sizeof(uint8_t));
+    check_ptr_not_null(allocated_memory, "Could not allocate and initialise RAM memory");
+    cpu_state->memory = (uint8_t *) allocated_memory;
 
-	return cpu_state;
+    return cpu_state;
 }
 
 
 void cpu_state_free(CpuState *cpu_state) {
-	free(cpu_state->memory);
-	free(cpu_state->registers);
-	free(cpu_state);
+    free(cpu_state->memory);
+    free(cpu_state->registers);
+    free(cpu_state);
 }
 
 
 void set_flag(CpuState *cpuState, flag flag, bool set) {
-	if (flag > 3){
-		print_error_exit("Invalid flag.");
-	}
-	int mask = (1 << (MAX_INDEX - flag));
-	cpuState->registers[CPSR] = set ?
-			cpuState->registers[CPSR] | mask : cpuState->registers[CPSR] & ~(mask);
+    if (flag > 3){
+        print_error_exit("Invalid flag.");
+    }
+    int mask = (1 << (MAX_INDEX - flag));
+    cpuState->registers[CPSR] = set ?
+            cpuState->registers[CPSR] | mask : cpuState->registers[CPSR] & ~(mask);
 }
 
 
 bool get_flag(CpuState *cpuState, flag flag) {
-	int mask = (1 << (MAX_INDEX - flag));
-	return cpuState->registers[CPSR] & mask;
+    int mask = (1 << (MAX_INDEX - flag));
+    return cpuState->registers[CPSR] & mask;
 }
 
 
 // Functions to print the state of the cpu (registers, memory)
 
 void print_registers(CpuState *cpu_state) {
-	printf("Registers:\n");
-	for (int i = 0; i < NUM_REGISTERS; i++) {
-		uint32_t val = cpu_state->registers[i];
-		if (i == 13 || i == 14) {
-			continue;
-		}
+    printf("Registers:\n");
+    for (int i = 0; i < NUM_REGISTERS; i++) {
+        uint32_t val = cpu_state->registers[i];
+        if (i == 13 || i == 14) {
+            continue;
+        }
 
-		if (i != CPSR && i != PC) {
-			if (i < 10) {
-				printf("$%d  :", i);
-			} else {
-				printf("$%d :", i);
-			}
-		}
-		else if (i == PC) {
-			printf("PC  :");
-		}
-		else {
-			printf("CPSR:");
-		}
-		if (val == 0x80000000) {  // weird edge case (test suite has inconsistent spacing)
-			printf(" ");
-		}
-		printf("%11d", val);
-		printf(" (0x%.8x)\n", val);
-	}
+        if (i != CPSR && i != PC) {
+            if (i < 10) {
+                printf("$%d  :", i);
+            } else {
+                printf("$%d :", i);
+            }
+        }
+        else if (i == PC) {
+            printf("PC  :");
+        }
+        else {
+            printf("CPSR:");
+        }
+        if (val == 0x80000000) {  // weird edge case (test suite has inconsistent spacing)
+            printf(" ");
+        }
+        printf("%11d", val);
+        printf(" (0x%.8x)\n", val);
+    }
 }
 
 
 void print_nonzero_little_endian_memory(CpuState *cpu_state, size_t bytes) {
-	// it is assumed memory is an array of bytes
-	// but addresses are all 32-bit
-	printf("Non-zero memory:\n");
-	for (int i = 0; i < bytes; i += 4){
-		uint32_t val = fetch(i, cpu_state);
-		if (val != 0X0){
-			printf("0x%.8x: 0x%.8x\n", i, val);
-		}
-	}
+    // it is assumed memory is an array of bytes
+    // but addresses are all 32-bit
+    printf("Non-zero memory:\n");
+    for (int i = 0; i < bytes; i += 4){
+        uint32_t val = fetch(i, cpu_state);
+        if (val != 0X0){
+            printf("0x%.8x: 0x%.8x\n", i, val);
+        }
+    }
 }
 
 
 void print_nonzero_big_endian_memory(CpuState *cpu_state, size_t bytes) {
-	printf("Non-zero memory:\n");
-	for (int i = 0; i < bytes; i += 4) {
-		uint8_t *memory = cpu_state->memory;
-		uint8_t byte1 = memory[i];
-		uint8_t byte2 = memory[i+1];
-		uint8_t byte3 = memory[i+2];
-		uint8_t byte4 = memory[i+3];
-		uint32_t val = (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
-		if (val == 0x0) {
-			continue;
-		}
-		printf("0x%.8x: 0x%.8x\n", i, val);
-	}
+    printf("Non-zero memory:\n");
+    for (int i = 0; i < bytes; i += 4) {
+        uint8_t *memory = cpu_state->memory;
+        uint8_t byte1 = memory[i];
+        uint8_t byte2 = memory[i+1];
+        uint8_t byte3 = memory[i+2];
+        uint8_t byte4 = memory[i+3];
+        uint32_t val = (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+        if (val == 0x0) {
+            continue;
+        }
+        printf("0x%.8x: 0x%.8x\n", i, val);
+    }
 }
 
 
 static uint32_t *get_pc(CpuState *cpu_state) {
-	return (cpu_state->registers) + PC;
+    return (cpu_state->registers) + PC;
 }
 
 
 uint32_t *offset_pc(int32_t offset, CpuState *cpu_state) {
-	uint32_t *pc = get_pc(cpu_state); 
-	*pc = ((int32_t) *pc + offset);
-	return pc;
+    uint32_t *pc = get_pc(cpu_state); 
+    *pc = ((int32_t) *pc + offset);
+    return pc;
 }
 
 
 uint32_t *increment_pc(CpuState *cpu_state) {
-	uint32_t *pc = get_pc(cpu_state); 
-	*pc = *pc + 4;
-	return pc;
+    uint32_t *pc = get_pc(cpu_state); 
+    *pc = *pc + 4;
+    return pc;
 }
--- src/emulator/single_data_transfer_instr.c
+++ src/emulator/single_data_transfer_instr.c
@@ -15,66 +15,66 @@
 
 
 void execute_single_data_transfer_instr(CpuState *cpu_state, Instruction *instr) {
-	assert(TRANSFER_REG_BITS < NUM_REGISTERS);
+    assert(TRANSFER_REG_BITS < NUM_REGISTERS);
 
-	uint16_t offset = compute_offset(cpu_state, instr);
-	uint32_t address = compute_address(cpu_state, instr, offset);
+    uint16_t offset = compute_offset(cpu_state, instr);
+    uint32_t address = compute_address(cpu_state, instr, offset);
 
-	bool valid = check_valid_memory_access(cpu_state, address);
-	if (!valid) {
-		return;
-	}
+    bool valid = check_valid_memory_access(cpu_state, address);
+    if (!valid) {
+        return;
+    }
 
-	// Load/store based on the transfer operation bit, rearranging for little endian
-	if (TRANSFER_TYPE_BIT) {
-		uint8_t b1 = cpu_state->memory[address];
-		uint8_t b2 = cpu_state->memory[address + 1];
-		uint8_t b3 = cpu_state->memory[address + 2];
-		uint8_t b4 = cpu_state->memory[address + 3];
-		cpu_state->registers[TRANSFER_REG_BITS] = b1 | (b2 << 8) | (b3 << 16) | (b4 << 24);
-	} else {
-		uint32_t reg_val = cpu_state->registers[TRANSFER_REG_BITS];
-		cpu_state->memory[address] = process_mask(reg_val, 0, 7);
-		cpu_state->memory[address + 1] = process_mask(reg_val, 8, 15);
-		cpu_state->memory[address + 2] = process_mask(reg_val, 16, 23);
-		cpu_state->memory[address + 3] = process_mask(reg_val, 24, 31);
-	}
+    // Load/store based on the transfer operation bit, rearranging for little endian
+    if (TRANSFER_TYPE_BIT) {
+        uint8_t b1 = cpu_state->memory[address];
+        uint8_t b2 = cpu_state->memory[address + 1];
+        uint8_t b3 = cpu_state->memory[address + 2];
+        uint8_t b4 = cpu_state->memory[address + 3];
+        cpu_state->registers[TRANSFER_REG_BITS] = b1 | (b2 << 8) | (b3 << 16) | (b4 << 24);
+    } else {
+        uint32_t reg_val = cpu_state->registers[TRANSFER_REG_BITS];
+        cpu_state->memory[address] = process_mask(reg_val, 0, 7);
+        cpu_state->memory[address + 1] = process_mask(reg_val, 8, 15);
+        cpu_state->memory[address + 2] = process_mask(reg_val, 16, 23);
+        cpu_state->memory[address + 3] = process_mask(reg_val, 24, 31);
+    }
 }
 
 
 uint16_t compute_offset(CpuState *cpu_state, Instruction *instr) {
-	uint16_t offset;
-	if (IMMEDIATE_BIT) {
-		// Register shifted offset (as in data processing type instructions)
-		uint8_t carry = 0;
-		offset = reg_offset_shift(cpu_state, instr, &carry);
-	} else {
-		offset = OFFSET_BITS;
-	}
+    uint16_t offset;
+    if (IMMEDIATE_BIT) {
+        // Register shifted offset (as in data processing type instructions)
+        uint8_t carry = 0;
+        offset = reg_offset_shift(cpu_state, instr, &carry);
+    } else {
+        offset = OFFSET_BITS;
+    }
 
-	return offset;
+    return offset;
 }
 
 
 uint32_t compute_address(CpuState *cpu_state, Instruction *instr, uint16_t offset) {
-	uint32_t address;
-	uint32_t base_reg_val = cpu_state->registers[BASE_REG_BITS];
+    uint32_t address;
+    uint32_t base_reg_val = cpu_state->registers[BASE_REG_BITS];
 
-	if (UP_BIT) {                                          // up bit is set
-		if (INDEXING_BIT) {                                    // pre-indexing
-			address = base_reg_val + offset;
-		} else {                                               // post-indexing
-			address = base_reg_val;
-			cpu_state->registers[BASE_REG_BITS] += offset; 
-		}
-	} else {                                               // up bit not set
-		if (INDEXING_BIT) {                                    // pre-indexing
-			address = base_reg_val - offset;
-		} else {                                               // post-indexing
-			address = base_reg_val;
-			cpu_state->registers[BASE_REG_BITS] -= offset; 
-		}
-	}
+    if (UP_BIT) {                                          // up bit is set
+        if (INDEXING_BIT) {                                    // pre-indexing
+            address = base_reg_val + offset;
+        } else {                                               // post-indexing
+            address = base_reg_val;
+            cpu_state->registers[BASE_REG_BITS] += offset; 
+        }
+    } else {                                               // up bit not set
+        if (INDEXING_BIT) {                                    // pre-indexing
+            address = base_reg_val - offset;
+        } else {                                               // post-indexing
+            address = base_reg_val;
+            cpu_state->registers[BASE_REG_BITS] -= offset; 
+        }
+    }
 
-	return address;
+    return address;
 }
