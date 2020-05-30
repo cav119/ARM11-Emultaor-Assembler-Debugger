148e19289b9939fae7c7d24bf1f8f79dfe66a709
Mihnea <mrg119@ic.ac.uk>
2020-05-30 11:35:33 +0300

    converted hash to generic hash

--- src/assemble.c
+++ src/assemble.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include "assembler/str_to_int_hash_tb.h"
 
 
 int count_lines(FILE *file){
@@ -73,7 +73,40 @@ return n;
 
 }
 
+int str_cmp(const void *str1, const void *str2){
+   char *string1 = (char *) str1;
+   char *string2 = (char *) str2;
+   return strcmp(string1, string2) == 0; 
+}
+
+int int_cmp(const void *p1, const void *p2){
+    int *int1 = (int *) p1;
+    int *int2 = (int *) p2;
+    return *int1 == *int2;
+}
+
+int ip(int *key, Dict d){
+    int *intptr = dict_search(d, key);
+    return *intptr;
+}
+
+
+void dict_test(void){
+   Dict d = dict_create(int_cmp);
+   int i1 = 3;
+   int i2 = 19;
+   int foo = 1;
+   dict_insert(d, &foo, &i1);
+    printf("%d\n", ip(&foo, d));
+    dict_insert(d, &foo, &i2);
+   printf("%d\n", ip(&foo, d));
+   dict_delete(d, &foo);
+   dict_destroy(d); 
+
+}
+
 int main(int argc, char **argv) {
+    dict_test();
     assert (argc == 3);
     FILE *file = fopen(argv[1], "r");
 
--- src/assembler/str_to_int_hash_tb.h
+++ src/assembler/str_to_int_hash_tb.h
@@ -1,22 +1,46 @@
 #include <stdint.h>
 
-typedef struct dict* Dict;
+//base functionality imported from
+//  http://www.cs.yale.edu/homes/aspnes/pinewiki/C(2f)HashTables.html
 
+
+/* Creates a new Dictionary (~hashmap) with any type as key or value 
+//    but always operatoes on pointers, so if you want (int, int)
+//    you'll have to use (*int, *int)
+
+*/
+
+typedef struct dict *Dict;
+typedef void *hashkey;
+typedef void *hashvalue;
+
+struct elt {
+    struct elt *next;
+    hashkey key;
+    hashvalue value;
+};
+
+struct dict {
+    int size;           /* size of the pointer table */
+    int n;              /* number of elements stored */
+    int (*comp)(const void *, const void *);
+    struct elt **table;
+};
 
 
 /* create a new empty dictionary */
-Dict dict_create(void);
+Dict dict_create(int (*comp)(const void *, const void *));
 
 /* destroy a dictionary */
 void dict_destroy(Dict);
 
 /* insert a new key-value pair into an existing dictionary */
-void dict_insert(Dict, const char *key, uint32_t value);
+void dict_insert(Dict, hashkey key, hashvalue value);
 
 /* return the most recently inserted value associated with a key */
 /* or 0 if no matching key is present */
-uint32_t dict_search(Dict, const char *key);
+void * dict_search(Dict, hashkey key);
 
 /* delete the most recently inserted record with the given key */
 /* if there is no such record, has no effect */
-void dict_delete(Dict, const char *key);
+void dict_delete(Dict, hashkey key);
