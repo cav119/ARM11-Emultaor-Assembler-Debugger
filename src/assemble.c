b3f2ee45d457f377ab4f07ef3d3c9ad7c43d425c
Mihnea <mrg119@ic.ac.uk>
2020-05-31 17:22:08 +0300

    adapted branch instr to new hashmap

--- src/assemble.c
+++ src/assemble.c
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-
+#include "assembler/asm_utilities.h"
+#include "assembler/hash_table.h"
 
 int count_lines(FILE *file){
     int count = 0;
@@ -44,11 +45,6 @@ char **line_to_words(char array[]){
     return words;
 }
 
-int32_t setBit(uint32_t n, int pos){
-    n |= (1 << pos);
-    return n;
-}
-
 
 int read_red_num(char *reg){
     if (reg == 0){
@@ -57,26 +53,40 @@ int read_red_num(char *reg){
     return  atoi(strtok(reg, "r"));
 }
 
-uint32_t encode_multiply(char *instr[]){
-    uint32_t n = (111 << 29);
-    n = setBit(n, 4);
-    n = setBit(n, 7);
-    n |= (read_red_num(instr[1]) << 16);
-    n|= read_red_num(instr[2]);
-    n|= (read_red_num(instr[3]) << 8);
 
-    if (strcasecmp(instr[0], "mull") != 0){
-        n = setBit(n, 21);
-        n|= (read_red_num(instr[4]) << 12);
-    }
-return n;
+int str_cmp(const void *arg1, const void *arg2){
+    char *str1 = (char *) arg1;
+    char *str2 = (char *) arg2;
+    return strcmp(str1, str2) == 0;
+
+}
+
+int int_ptr_cmp(const void *arg1, const void *arg2){
+    char *i1 = (int *) arg1;
+    char *i2 = (int *) arg2;
+    return *i1 == *i2;
+}
+
+void test_ht(void){
+    HashTable *ht = ht_create(int_ptr_cmp);
+    int *k1 = malloc(sizeof(int));
+    *k1 = 12;
+    size_t size1 = sizeof(int);
+    ht_set(ht, k1, strdup("asdasd"), size1);
+   // printf("%s\n", ht_get(ht, k1, size1));
+    ht_set(ht, k1, strdup("mikeanike"), size1);
+   //  printf("%s\n", ht_get(ht, k1, size1));
+    ht_del(ht, k1, size1);
+    ht_free(ht);
+
 
 }
 
 int main(int argc, char **argv) {
+    test_ht();
     assert (argc == 3);
     FILE *file = fopen(argv[1], "r");
-
+    check_pointer(file, "File not found");
     int lines = count_lines(file);
     char **array = parse_to_array(file, lines);
     fclose(file);
--- src/assembler/asm_branch_instr.c
+++ src/assembler/asm_branch_instr.c
@@ -16,52 +16,58 @@
 #define AL_CODE (14)
 
 // comprator function for hash, required by dict_create
-static uint32_t compare_str(const void *s1, const void *s2){
+static int compare_str(const void *s1, const void *s2){
     char *str1 = (char *) s1;
     char *str2 = (char *) s2;
     return strcmp(str1, str2) == 0;
 }
 
-static Dict init_rule_hash(){
-    Dict hash_table = dict_create(compare_str);
+static size_t str_size(char *str){
+    return sizeof(char) * (strlen(str) + 1);
+}
+
+static HashTable *init_rule_hash(){
+    HashTable *hash_table = ht_create(compare_str);
     // Inserts the suffixes and corresponding codes
     // for the branch instruction
-    uint32_t *eq_ptr = malloc(sizeof(uint32_t *));
+    size_t code_str_size = sizeof(char) * 3;
+    size_t ui32 = sizeof(uint32_t);
+
+    uint32_t *eq_ptr = malloc(ui32);
     *eq_ptr = EQ_CODE;
 
-    uint32_t *ne_ptr = malloc(sizeof(uint32_t *));
+    uint32_t *ne_ptr = malloc(ui32);
     *ne_ptr = NE_CODE;
 
-
-    uint32_t *ge_ptr = malloc(sizeof(uint32_t *));
+    uint32_t *ge_ptr = malloc(ui32);
     *ge_ptr = GE_CODE;
 
-    uint32_t *lt_ptr = malloc(sizeof(uint32_t *));
+    uint32_t *lt_ptr = malloc(ui32);
     *lt_ptr = LT_CODE;
 
-    uint32_t *gt_ptr = malloc(sizeof(uint32_t *));
+    uint32_t *gt_ptr = malloc(ui32);
     *gt_ptr = GT_CODE;
 
-    uint32_t *le_ptr = malloc(sizeof(uint32_t *));
+    uint32_t *le_ptr = malloc(ui32);
     *le_ptr = LE_CODE;
 
-    uint32_t *al_ptr = malloc(sizeof(uint32_t *));
+    uint32_t *al_ptr = malloc(ui32);
     *al_ptr = AL_CODE;
 
-    dict_insert(hash_table, strdup("eq") ,eq_ptr);
-    dict_insert(hash_table, strdup("ne"), ne_ptr);
-    dict_insert(hash_table, strdup("ge"), ge_ptr);
-    dict_insert(hash_table, strdup("lt"), lt_ptr);
-    dict_insert(hash_table, strdup("gt"), gt_ptr);
-    dict_insert(hash_table, strdup("le"), le_ptr);
-    dict_insert(hash_table, strdup("al"), al_ptr);
+    ht_set(hash_table, strdup("eq") ,eq_ptr, ui32);
+    ht_set(hash_table, strdup("ne"), ne_ptr, ui32);
+    ht_set(hash_table, strdup("ge"), ge_ptr, ui32);
+    ht_set(hash_table, strdup("lt"), lt_ptr, ui32);
+    ht_set(hash_table, strdup("gt"), gt_ptr, ui32);
+    ht_set(hash_table, strdup("le"), le_ptr, ui32);
+    ht_set(hash_table, strdup("al"), al_ptr, ui32);
     return hash_table;
 }
 
 
-Instruction *decode_branch_instr_to_bin(char code[5][512], Dict label_table
-                , Dict waiting_labels, int current_line){
-    Dict codes_maps = init_rule_hash();
+Instruction *decode_branch_instr_to_bin(char code[5][512], HashTable *label_table
+                , HashTable *waiting_labels, int current_line){
+    HashTable *codes_maps = init_rule_hash();
     char *rule = strdup(code[0]);
     if (rule[0] != 'b'){
         // must be a label, not a jump
@@ -72,7 +78,7 @@ Instruction *decode_branch_instr_to_bin(char code[5][512], Dict label_table
             that we hit a label and the next valid instruction that is reached
             should be the pointer, which would be way better
         if (label_table != NULL){
-            dict_insert(label_table, strdup(label), current_line+4); 
+            ht_set(label_table, strdup(label), current_line+4); 
         }
 
         */
@@ -82,13 +88,13 @@ Instruction *decode_branch_instr_to_bin(char code[5][512], Dict label_table
     else {
         // get the condition
         char cond[2] = {code[0][1], code[0][2], '\0'};
-        uint32_t int_code = *( (int *)dict_search(codes_maps, cond) );
+        uint32_t int_code = *( (int *)ht_get(codes_maps, cond, sizeof(char) * 3) );
         // see the label
         char *label = strdup(code[1]);
         //check against list of labels and see whether it's inside
         uint32_t instr = int_code << 28;
         instr |= 10 << 24; // 1010 at bits 27-24
-        if (label_table != NULL && dict_search(label_table, label)){
+        if (label_table != NULL && ht_get(label_table, label, str_size(label))){
             // calculate offset according to current line number
             // and offset line number and subtract 8 
         }
@@ -100,7 +106,7 @@ Instruction *decode_branch_instr_to_bin(char code[5][512], Dict label_table
         }
         free(label);
     }
-    dict_destroy(codes_maps);
+    ht_free(codes_maps);
     free(rule);
     return 0;
 }
